bbc78df29dfff5cac03266e658f0049f51f78017
zstar4004-star <zstar4004@gmail.com>
2026-07-01 13:46:29 +0300

    Update infinite_canvas.dart

--- lib/widgets/infinite_canvas.dart
+++ lib/widgets/infinite_canvas.dart
@@ -1,18 +1,62 @@
 import 'package:flutter/material.dart';
 
-class InfiniteCanvas extends StatelessWidget {
+import '../controllers/canvas_controller.dart';
+import '../painters/grid_painter.dart';
+
+class InfiniteCanvas extends StatefulWidget {
   const InfiniteCanvas({super.key});
 
   @override
+  State<InfiniteCanvas> createState() => _InfiniteCanvasState();
+}
+
+class _InfiniteCanvasState extends State<InfiniteCanvas> {
+  final CanvasController controller = CanvasController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xff1b1b1b),
-      child: const Center(
-        child: Text(
-          "Infinite Canvas",
-          style: TextStyle(fontSize: 22),
-        ),
-      ),
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (_, __) {
+        return InteractiveViewer(
+          transformationController: controller.transformationController,
+          boundaryMargin: const EdgeInsets.all(double.infinity),
+          minScale: 0.1,
+          maxScale: 8,
+          panEnabled: true,
+          scaleEnabled: true,
+          constrained: false,
+          child: SizedBox(
+            width: 10000,
+            height: 10000,
+            child: Stack(
+              children: [
+                if (controller.showGrid)
+                  CustomPaint(
+                    size: const Size(10000, 10000),
+                    painter: GridPainter(),
+                  ),
+
+                const Center(
+                  child: Text(
+                    "Canvas Ready",
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
