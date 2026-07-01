949aee6452fafd3102a8c8a250fbabd1687f9520
zstar4004-star <zstar4004@gmail.com>
2026-07-01 14:09:36 +0300

    Update infinite_canvas.dart

--- lib/widgets/infinite_canvas.dart
+++ lib/widgets/infinite_canvas.dart
@@ -1,62 +1,51 @@
 import 'package:flutter/material.dart';
 
 import '../controllers/canvas_controller.dart';
-import '../painters/grid_painter.dart';
+import '../controllers/editor_controller.dart';
+import 'tree_canvas.dart';
 
 class InfiniteCanvas extends StatefulWidget {
-  const InfiniteCanvas({super.key});
+  const InfiniteCanvas({
+    super.key,
+  });
 
   @override
-  State<InfiniteCanvas> createState() => _InfiniteCanvasState();
+  State<InfiniteCanvas> createState() =>
+      _InfiniteCanvasState();
 }
 
-class _InfiniteCanvasState extends State<InfiniteCanvas> {
-  final CanvasController controller = CanvasController();
+class _InfiniteCanvasState
+    extends State<InfiniteCanvas> {
+  final CanvasController canvas =
+      CanvasController();
+
+  final EditorController editor =
+      EditorController();
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller,
-      builder: (_, __) {
-        return InteractiveViewer(
-          transformationController: controller.transformationController,
-          boundaryMargin: const EdgeInsets.all(double.infinity),
-          minScale: 0.1,
-          maxScale: 8,
-          panEnabled: true,
-          scaleEnabled: true,
-          constrained: false,
-          child: SizedBox(
-            width: 10000,
-            height: 10000,
-            child: Stack(
-              children: [
-                if (controller.showGrid)
-                  CustomPaint(
-                    size: const Size(10000, 10000),
-                    painter: GridPainter(),
-                  ),
-
-                const Center(
-                  child: Text(
-                    "Canvas Ready",
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 30,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
+    return InteractiveViewer(
+      transformationController:
+          canvas.transformationController,
+      boundaryMargin:
+          const EdgeInsets.all(double.infinity),
+      constrained: false,
+      minScale: 0.1,
+      maxScale: 8,
+      child: SizedBox(
+        width: 10000,
+        height: 10000,
+        child: TreeCanvas(
+          controller: editor,
+        ),
+      ),
     );
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    canvas.dispose();
+    editor.dispose();
     super.dispose();
   }
 }
