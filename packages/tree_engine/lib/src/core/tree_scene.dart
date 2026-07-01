bccc0b23c362b8962ebd597e990eac11890ba19f
zstar4004-star <zstar4004@gmail.com>
2026-07-01 14:35:01 +0300

    Update tree_scene.dart

--- packages/tree_engine/lib/src/core/tree_scene.dart
+++ packages/tree_engine/lib/src/core/tree_scene.dart
@@ -2,20 +2,25 @@ import 'tree_node.dart';
 import 'node_type.dart';
 
 class TreeScene {
+  TreeScene();
+
   final List<TreeNode> _roots = [];
 
   List<TreeNode> get roots => List.unmodifiable(_roots);
 
-  bool get hasTrunk {
-    return _roots.any(
-      (e) => e.type == NodeType.trunk,
-    );
-  }
+  bool get hasTrunk =>
+      _roots.any((e) => e.type == NodeType.trunk);
 
   void addRoot(TreeNode node) {
     if (node.type == NodeType.trunk && hasTrunk) {
-      throw Exception(
-        "Only one trunk is allowed.",
+      throw StateError(
+        'Only one trunk is allowed in the scene.',
+      );
+    }
+
+    if (node.parent != null) {
+      throw StateError(
+        'Root node cannot already have a parent.',
       );
     }
 
@@ -40,9 +45,38 @@ class TreeScene {
     }
   }
 
+  TreeNode? findById(String id) {
+    for (final node in traverse()) {
+      if (node.id == id) {
+        return node;
+      }
+    }
+
+    return null;
+  }
+
+  bool contains(TreeNode node) {
+    return traverse().contains(node);
+  }
+
   void clearSelection() {
     for (final node in traverse()) {
       node.selected = false;
     }
+  }
+
+  List<TreeNode> selectedNodes() {
+    return traverse()
+        .where((e) => e.selected)
+        .toList(growable: false);
+  }
+
+  void remove(TreeNode node) {
+    if (node.parent != null) {
+      node.parent!.removeChild(node);
+      return;
+    }
+
+    _roots.remove(node);
   }
 }
